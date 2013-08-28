ef4dee399767aaf159622ca0b2f0ec35b264dbdc
Kristofer Rye <kristofer.rye@gmail.com>
2013-08-28 15:02:26 -0400

    improved prime verification algorithm
    @cg505, yes, I included a call to get-n-primes in the base-level, but
    until I get a working "slime" installation, I don't think I'll be
    removing that simply so I can clisp it (unless you give a better
    alternative)

--- Primes/Generate N Primes/LISP/main.lisp
+++ Primes/Generate N Primes/LISP/main.lisp
@@ -2,14 +2,20 @@
   (= (floor number x) (/ number x)))
 
 (defun prime? (number primes-to)
-  (not (some #'(lambda (x) (divisible-by number x))
-			 primes-to)))
+  (dolist (prime primes-to)
+    (if (>= prime (sqrt number))
+        (return t))
+    (if (divisible-by number prime)
+        (return f)))
+  t)
 
 (defun get-n-primes (cap)
   (let ((primes nil)
-		(count 2)) ; the number we're testing
-	(loop while (> cap (length primes)) ; loop while we don't still have enough primes
-		  do (when (prime? count primes)
-			   (setf primes (cons count primes)))
-		  do (incf count))
-	primes))
+        (count 2)) ; the number we're testing
+    (loop while (> cap (length primes)) ; loop while we don't still have enough primes
+          do (when (prime? count primes)
+               (setf primes (cons count primes)))
+          do (incf count))
+    primes))
+
+(get-n-primes 2000000)
